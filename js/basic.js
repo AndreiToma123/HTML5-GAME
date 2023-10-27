c2a2e18fc66e68b162dee43a3612ddeec773be01
AndreiToma123 <Andrei.Toma@student.lut.fi>
2023-10-27 11:03:59 +0300

    .

--- js/basic.js
+++ js/basic.js
@@ -49,22 +49,41 @@ class PlayGame extends Phaser.Scene {
         this.load.image("bomb", "assets/items/bomb.png");
         this.load.image("bullet", "assets/items/ninja_star.png");
         this.load.image('portal', 'assets/world/portal.png');
+        this.load.audio('coin', "assets/sound_effects/coin.mp3");
+        this.load.audio('jump', "assets/sound_effects/jump.mp3");
+        this.load.audio('shoot', "assets/sound_effects/shoot.mp3");
+        this.load.audio('soundtrack', "assets/sound_effects/soundtrack.mp3");
+        this.load.audio('teleport', "assets/sound_effects/teleport.mp3");
     }
 
     create(){
-        console.log(this.scene.isActive('Scene2'));
+        // this.backgroundMusic = this.sound.add("soundtrack", {loop: true}); NEED THIS
+        // this.backgroundMusic.play();
+
+        this.coin_sound = this.sound.add('coin');
+        this.jump_sound = this.sound.add('jump');
+        this.shoot_sound = this.sound.add('shoot');
+        this.teleport_sound = this.sound.add('teleport');
+
         this.background = this.add.image(0, 0, 'background').setOrigin(0, 0);
+
+        this.scoreText = this.add.text(8, 3, "0", {fontSize: "50px", fill: "#ffffff"})
+
+        // this.portal = this.physics.add.image(400, 840, 'portal').setScale(5);
+        // this.portal.setOrigin(0.5, 0.5);
+        // this.physics.add.existing(this.portal);
+
         this.groundGroup = this.physics.add.group({
             immovable: true,
             allowGravity: false,
         })
 
-        this.portal = this.physics.add.image(400, 840, 'portal').setScale(5);
-        this.portal.setOrigin(0.5, 0.5);
-        this.physics.add.existing(this.portal);
+        this.moveableGroundGroup = this.physics.add.group({
+            immovable: true,
+            allowGravity: false,
+        })
 
-        this.scoreText = this.add.text(8, 3, "0", {fontSize: "50px", fill: "#ffffff"})
-        // this.instructionText = this.add.text(490, 3, "Controls: UP, RIGHT, LEFT", {fontSize: "20px", fill: "#ffffff"})
+       
 
         this.groundGroup.create(60, 970, 'ground');
         this.groundGroup.create(180, 970, 'ground');
@@ -74,31 +93,38 @@ class PlayGame extends Phaser.Scene {
         this.groundGroup.create(660, 970, 'ground');
         this.groundGroup.create(780, 970, 'ground');
 
+        for (let i = 0; i < 3; i++) {
+            const movingPlatform = this.moveableGroundGroup.create(900 + i * 100, 970, 'ground'); 
+            movingPlatform.setVelocityX(100); 
+        }
+
         this.monkey = this.physics.add.sprite(50, 870, 'monkey').setScale(0.2);
         this.monkey.body.setCollideWorldBounds(true);
         this.monkey.body.gravity.y = gameOption.monkeyGravity;
         this.physics.add.collider(this.monkey, this.groundGroup);
+        this.physics.add.collider(this.monkey, this.moveableGroundGroup);
         this.monkey.setSize(250, 200, true);
 
         this.fruitGroup = this.physics.add.group({})
-        this.physics.add.overlap(this.monkey, this.fruitGroup, this.collectFruit, null, this);
-        this.physics.add.overlap(this.groundGroup, this.bombGroup, this.explodeBomb, null, this);
-        this.physics.add.overlap(this.monkey, this.portal, this.teleportPlayer, null, this);
-        
-        
-        // this.bulletGroup = this.physics.add.group({
-        //     defaultKey: 'bullet',
-        //     maxSize: 1
-        // })
+
+        this.bulletGroup = this.physics.add.group({
+            defaultKey: 'bullet',
+            maxSize: 1
+        })
 
         // this.physics.add.overlap(this.bombGroup, this.bulletGroup, this.defuseBomb, null, this);
 
         this.cursors = this.input.keyboard.createCursorKeys();
 
+        this.physics.add.overlap(this.monkey, this.fruitGroup, this.collectFruit, null, this);
+        this.physics.add.overlap(this.groundGroup, this.bombGroup, this.explodeBomb, null, this);
+        this.physics.add.overlap(this.monkey, this.portal, this.teleportPlayer, null, this);
+        
 }
 
     collectFruit(monkey, fruit) {
         fruit.disableBody(true, true);
+        this.coin_sound.play();
         if(fruit.texture.key === "pear"){
             this.score += 1;
             this.scoreText.setText(this.score);
@@ -113,16 +139,18 @@ class PlayGame extends Phaser.Scene {
         }
     }
 
-    teleportPlayer() {
-        this.scene.start('Scene2');
+    shootBullet() {
+            this.shoot_sound.play();
+            this.bulletGroup.get(this.monkey.x, this.monkey.y);
+            this.bulletGroup.setActive(true);
+            this.bulletGroup.setVisible(true);
+            this.bulletGroup.setVelocityX(600);
     }
 
-    // shootBullet() {
-    //         this.bulletGroup.get(this.monkey.x, this.monkey.y);
-    //         this.bulletGroup.setActive(true);
-    //         this.bulletGroup.setVisible(true);
-    //         this.bulletGroup.setVelocityY(-200);
-    // }
+    teleportPlayer() {
+        this.teleport_sound.play();
+        this.scene.start('Scene2');
+    }
 
     update() {
 
@@ -133,16 +161,31 @@ class PlayGame extends Phaser.Scene {
             this.monkey.body.velocity.x = gameOption.monkeySpeed;
             this.monkey.flipX = true;
         }
-        // else if(this.cursors.up.isDown){
-        //     this.shootBullet();
-        // }
+        else if(this.input.activePointer.isDown && this.input.activePointer.leftButtonDown()){
+            this.shootBullet();
+        }
         else{
             this.monkey.body.velocity.x = 0;
         }
 
         if (this.cursors.up.isDown && this.monkey.body.touching.down) {
             this.monkey.body.velocity.y = -gameOption.monkeyGravity / 2.5;
+            this.jump_sound.play();
         }
+
+        this.bulletGroup.children.iterate(bullet => {
+            if (bullet.x < 0 || bullet.x > this.game.config.width) {
+                bullet.destroy(); 
+            }
+        });
+
+        this.moveableGroundGroup.getChildren().forEach(movingPlatform => {
+            if (movingPlatform.x >= 1600) {
+                movingPlatform.setVelocityX(-100);
+            } else if (movingPlatform.x <= 900) {
+                movingPlatform.setVelocityX(100);
+            }
+        });
     }
 
     gameOver() {
@@ -150,6 +193,8 @@ class PlayGame extends Phaser.Scene {
     }
 
 }
+
+
 
 class Scene2 extends Phaser.Scene {
     constructor() {
@@ -168,36 +213,88 @@ class Scene2 extends Phaser.Scene {
         this.load.image("bomb", "assets/items/bomb.png");
         this.load.image("bullet", "assets/items/ninja_star.png");
         this.load.image('portal', 'assets/world/portal.png');
+        this.load.audio('coin', "assets/sound_effects/coin.mp3");
+        this.load.audio('jump', "assets/sound_effects/jump.mp3");
+        this.load.audio('shoot', "assets/sound_effects/shoot.mp3");
+        this.load.audio('soundtrack', "assets/sound_effects/soundtrack.mp3");
+        this.load.audio('teleport', "assets/sound_effects/teleport.mp3");
     }
   
     create() {
+       
+        this.coin_sound = this.sound.add('coin');
+        this.jump_sound = this.sound.add('jump');
+        this.shoot_sound = this.sound.add('shoot');
+        this.teleport_sound = this.sound.add('teleport');
+
+        this.background = this.add.image(0, 0, 'background').setOrigin(0, 0);
+        this.groundGroup = this.physics.add.group({
+            immovable: true,
+            allowGravity: false,
+        })
+
+        this.scoreText = this.add.text(8, 3, "0", {fontSize: "50px", fill: "#ffffff"})
+
+        this.portal = this.physics.add.image(1550, 840, 'portal').setScale(5);
+        this.portal.setOrigin(0.5, 0.5);
+        this.physics.add.existing(this.portal);
+
+        this.groundGroup.create(60, 970, 'ground');
+        this.groundGroup.create(180, 970, 'ground');
         
 
-  
-      this.background = this.add.image(0, 0, 'background').setOrigin(0, 0);
-      this.groundGroup = this.physics.add.group({
-        immovable: true,
-        allowGravity: false,
-      })
-  
-      this.portal = this.physics.add.image(400, 840, 'portal').setScale(5);
-      this.portal.setOrigin(0.5, 0.5);
-      this.physics.add.existing(this.portal);
-  
-      this.scoreText = this.add.text(8, 3, "0", {fontSize: "50px", fill: "#ffffff"})
-  
-      this.groundGroup.create(60, 970, 'ground');
-      this.groundGroup.create(180, 970, 'ground');
-  
-      this.monkey = this.physics.add.sprite(50, 870, 'monkey').setScale(0.2);
-      this.monkey.body.setCollideWorldBounds(true);
-      this.monkey.body.gravity.y = gameOption.monkeyGravity;
-      this.physics.add.collider(this.monkey, this.groundGroup);
-      this.monkey.setSize(250, 200, true);
-  
-      this.cursors = this.input.keyboard.createCursorKeys();
+        this.monkey = this.physics.add.sprite(50, 870, 'monkey').setScale(0.2);
+        this.monkey.body.setCollideWorldBounds(true);
+        this.monkey.body.gravity.y = gameOption.monkeyGravity;
+        this.physics.add.collider(this.monkey, this.groundGroup);
+        this.monkey.setSize(250, 200, true);
+
+        this.fruitGroup = this.physics.add.group({})
+
+        this.bulletGroup = this.physics.add.group({
+            defaultKey: 'bullet',
+            maxSize: 1
+        })
+
+        // this.physics.add.overlap(this.bombGroup, this.bulletGroup, this.defuseBomb, null, this);
+
+        this.cursors = this.input.keyboard.createCursorKeys();
+
+        this.physics.add.overlap(this.monkey, this.fruitGroup, this.collectFruit, null, this);
+        this.physics.add.overlap(this.groundGroup, this.bombGroup, this.explodeBomb, null, this);
+        this.physics.add.overlap(this.monkey, this.portal, this.teleportPlayer, null, this);
 
       this.visible = true;
+    }
+
+    collectFruit(monkey, fruit) {
+        fruit.disableBody(true, true);
+        this.coin_sound.play();
+        if(fruit.texture.key === "pear"){
+            this.score += 1;
+            this.scoreText.setText(this.score);
+        }
+        if(fruit.texture.key === "orange"){
+            this.score += 5;
+            this.scoreText.setText(this.score);
+        }
+        if(fruit.texture.key === "banana"){
+            this.score += 10;
+            this.scoreText.setText(this.score);
+        }
+    }
+
+    shootBullet() {
+            this.shoot_sound.play();
+            this.bulletGroup.get(this.monkey.x, this.monkey.y);
+            this.bulletGroup.setActive(true);
+            this.bulletGroup.setVisible(true);
+            this.bulletGroup.setVelocityY(-600);
+    }
+
+    teleportPlayer() {
+        this.teleport_sound.play();
+        this.scene.start('EndMenu');
     }
 
     
@@ -210,16 +307,23 @@ class Scene2 extends Phaser.Scene {
             this.monkey.body.velocity.x = gameOption.monkeySpeed;
             this.monkey.flipX = true;
         }
-        // else if(this.cursors.up.isDown){
-        //     this.shootBullet();
-        // }
+        else if(this.input.activePointer.isDown && this.input.activePointer.leftButtonDown()){
+            this.shootBullet();
+        }
         else{
             this.monkey.body.velocity.x = 0;
         }
 
         if (this.cursors.up.isDown && this.monkey.body.touching.down) {
             this.monkey.body.velocity.y = -gameOption.monkeyGravity / 2.5;
+            this.jump_sound.play();
         }
+
+        this.bulletGroup.children.iterate(bullet => {
+            if (bullet.y < 0 || bullet.y > this.game.config.height) {
+                bullet.destroy(); 
+            }
+        });
     }
   }
 
